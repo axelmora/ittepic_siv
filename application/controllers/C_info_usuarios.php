9311690ccfd2ddf1d929b29d2e9046109c5e5872
fernando <feranimaciones@gmail.com>
2017-09-03 12:34:57 -0600

    Comparacion de nueva contraseña, y mensaje de error

--- application/controllers/C_info_usuarios.php
+++ application/controllers/C_info_usuarios.php
@@ -1,149 +1,184 @@
 <?php
-
 if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-
+exit('No direct script access allowed');
 /**
- * 
- */
+*
+*/
 class C_info_usuarios extends CI_Controller {
-
-    public function __construct() {
-        parent::__construct();
-        $this->load->library(array('session'));
-        $this->load->helper(array('url'));
-        $this->load->model('m_usuarios');
+  public function __construct() {
+    parent::__construct();
+    $this->load->library(array('session','form_validation'));
+    $this->load->helper(array('url'));
+    $this->load->model('m_usuarios');
+  }
+  public function index() {
+    if ($this->session->userdata('perfil') == FALSE) {
+      redirect(base_url() . 'index.php/logeo');
     }
-
-    public function index() {
-        if ($this->session->userdata('perfil') == FALSE) {
-            redirect(base_url() . 'index.php/logeo');
-        }
-        if ($this->session->userdata('perfil') == 'jefevinculacion') {
-            $data['titulo'] = 'Bienvenido de nuevo ' . $this->session->userdata('perfil');
-            $this->load->view('Residencia/JefeVinculacion/inicio_residencia_vinculacion', $data);
-        } else {
-            $this->load->view('notienespermisos');
-        }
+    if ($this->session->userdata('perfil') == 'jefevinculacion') {
+      $data['titulo'] = 'Bienvenido de nuevo ' . $this->session->userdata('perfil');
+      $this->load->view('Residencia/JefeVinculacion/inicio_residencia_vinculacion', $data);
+    } else {
+      $this->load->view('notienespermisos');
     }
-
-    //ADMINISTRATIVO------------------------------------------------------------------------------
-    public function administrativo() {
-        if ($this->session->userdata('perfil') == FALSE) {
-            redirect(base_url() . 'index.php/logeo');
-        }
-        if ($this->session->userdata('perfil') == 'jefevinculacion' || $this->session->userdata('perfil') == 'jefeacademico' ||
-                $this->session->userdata('perfil') == 'coordinadorresidencia' || $this->session->userdata('perfil') == 'presidenteacademia' ||
-                $this->session->userdata('perfil') == 'coordinadorprogac' || $this->session->userdata('perfil') == 'jeferesidencia') {
-            $data['info'] = $this->session->userdata('perfil');
-            $data['info_usuario'] = $this->m_usuarios->consulta_info_administrativo($this->session->userdata('id_usuario'));
-            $this->load->view('v_info_administrativo', $data);
-        } else {
-            $this->load->view('notienespermisos');
-        }
+  }
+  //ADMINISTRATIVO------------------------------------------------------------------------------
+  public function administrativo() {
+    if ($this->session->userdata('perfil') == FALSE) {
+      redirect(base_url() . 'index.php/logeo');
     }
-
-    public function actualizar_administrativo() {
-        $data = array(
-            'nombre_usuariosistema' => $this->input->post('usuario'),
-            'correo' => $this->input->post('correo')
-        );
-        $this->m_usuarios->actualizar($this->session->userdata('id_usuario'), $data);
-        $data['info'] = $this->session->userdata('perfil');
-        $data['info_usuario'] = $this->m_usuarios->consulta_info_administrativo($this->session->userdata('id_usuario'));
-        $this->load->view('v_info_administrativo', $data);
+    if ($this->session->userdata('perfil') == 'jefevinculacion' || $this->session->userdata('perfil') == 'jefeacademico' ||
+    $this->session->userdata('perfil') == 'coordinadorresidencia' || $this->session->userdata('perfil') == 'presidenteacademia' ||
+    $this->session->userdata('perfil') == 'coordinadorprogac' || $this->session->userdata('perfil') == 'jeferesidencia') {
+      $data['info'] = $this->session->userdata('perfil');
+      $data['info_usuario'] = $this->m_usuarios->consulta_info_administrativo($this->session->userdata('id_usuario'));
+      $this->load->view('v_info_administrativo', $data);
+    } else {
+      $this->load->view('notienespermisos');
     }
+  }
 
-    //DOCENTES------------------------------------------------------------------------------
-    public function docente() {
-        if ($this->session->userdata('perfil') == FALSE) {
-            redirect(base_url() . 'index.php/logeo');
-        }
-        if ($this->session->userdata('perfil') == 'docente') {
-            $data['info'] = $this->session->userdata('perfil');
-            $data['nombres'] = $this->session->userdata('nombres');
-            $data['apellidos'] = $this->session->userdata('apellidos');
-            $data['info_usuario'] = $this->m_usuarios->consulta_info_docente($this->session->userdata('rfc'));
-            $this->load->view('v_info_docente', $data);
-        } else {
+  public function actualizar_administrativo() {
+    $data = array(
+      'nombre_usuariosistema' => $this->input->post('usuario'),
+      'correo' => $this->input->post('correo')
+    );
+    $this->m_usuarios->actualizar($this->session->userdata('id_usuario'), $data);
+    $data['info'] = $this->session->userdata('perfil');
+    $data['info_usuario'] = $this->m_usuarios->consulta_info_administrativo($this->session->userdata('id_usuario'));
+    $this->load->view('v_info_administrativo', $data);
+  }
 
-            $this->load->view('notienespermisos');
-        }
+  public function validarEditarContrasena() {
+    $this->load->library('form_validation');
+    $this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+    $this->form_validation->set_rules('contraacutal', 'Contraseña Actual', 'required|min_length[1]|max_length[20]');
+    $this->form_validation->set_rules('contranueva1', 'Contraseña nueva', 'required|min_length[1]|max_length[20]');
+    $this->form_validation->set_rules('contranueva2', 'Contraseña nueva repetida', 'required|min_length[1]|max_length[20]|matches[contranueva1]');
+    $this->form_validation->set_message('required', '<i class="material-icons ">error</i>El campo no puede ir vacío');
+    $this->form_validation->set_message('min_length', '<i class="material-icons ">error</i>El  campo debe tener al menos %s carácteres');
+    $this->form_validation->set_message('max_length', '<i class="material-icons ">error</i>El campo no puede tener más de %s carácteres');
+    $this->form_validation->set_message('matches', '<i class="material-icons ">error</i> Debe ser la misma contraseña.');
+    if ($this->form_validation->run() == TRUE) {
+
+      /*    $data = array(
+      'contenido_n' => $this->input->post('cnoticia'),
+      'fecha_noticia' => date('Y-m-d H:i:s'),
+      'titulo_n' => $this->input->post('tnoticia')
+    );*/
+    //Transfering data to Model
+    /*  $this->m_noticias->form_insert_residencia($data);
+    $data['message'] = 'Los datos se insertaron correctamente';
+
+    $data['noticiasResidencia'] = $this->m_noticias->shownoticiasResidencia();
+    $this->load->view('Residencia/v_noticias_agregar_quitar', $data);*/
+  } else {
+    if ($this->session->userdata('perfil') == FALSE) {
+      redirect(base_url() . 'index.php/logeo');
     }
-
-    function actualizar_docente() {
-        $datos = array(
-            //'contrasena' => sha1($this->input->post('contrasena'))
-            'contrasena' => $this->input->post('contrasena')
-        );
-        if ($this->m_usuarios->actualizar_info_docente($this->session->userdata('rfc'), $datos)) {
-            redirect(base_url() . 'index.php/c_info_usuarios/docente');
-        } else {
-            $this->load->view('notienespermisos');
-        }
+    if ($this->session->userdata('perfil') == 'jefevinculacion' || $this->session->userdata('perfil') == 'jefeacademico' ||
+    $this->session->userdata('perfil') == 'coordinadorresidencia' || $this->session->userdata('perfil') == 'presidenteacademia' ||
+    $this->session->userdata('perfil') == 'coordinadorprogac' || $this->session->userdata('perfil') == 'jeferesidencia')
+    {
+      $data['messageerror'] = '<i class="material-icons ">error</i> Las contraseñas fueron incorrectas intente de nuevo.';
+      $data['info'] = $this->session->userdata('perfil');
+      $data['info_usuario'] = $this->m_usuarios->consulta_info_administrativo($this->session->userdata('id_usuario'));
+      $this->load->view('v_info_administrativo', $data);
+    } else {
+      $this->load->view('notienespermisos');
     }
+    //echo "error";
+  }
+}
+//DOCENTES------------------------------------------------------------------------------
+public function docente() {
+  if ($this->session->userdata('perfil') == FALSE) {
+    redirect(base_url() . 'index.php/logeo');
+  }
+  if ($this->session->userdata('perfil') == 'docente') {
+    $data['info'] = $this->session->userdata('perfil');
+    $data['nombres'] = $this->session->userdata('nombres');
+    $data['apellidos'] = $this->session->userdata('apellidos');
+    $data['info_usuario'] = $this->m_usuarios->consulta_info_docente($this->session->userdata('rfc'));
+    $this->load->view('v_info_docente', $data);
+  } else {
 
-    //ALUMNOS------------------------------------------------------------------------------
-    public function alumno() {
-        if ($this->session->userdata('logged_in')) {
-            $session_data = $this->session->userdata('logged_in');
-            $data['username'] = $session_data['username'];
-            $data['nombre'] = $session_data['nombre'];
-            $data['id_carrera'] = $session_data['id_carrera'];
-            $data['id_semestre'] = $session_data['id_semestre'];
-            $data['plan_estudios'] = $session_data['plan_estudios'];
-            $data['sexo'] = $session_data['sexo'];
-            $data['telefono'] = $session_data['telefono'];
-            $data['domicilio'] = $session_data['domicilio'];
-            $data['semestre_cursado'] = $session_data['semestre_cursado'];
-            $data['creditos'] = $session_data['creditos'];
-            $data['porcentaje_avance'] = $session_data['porcentaje_avance'];
+    $this->load->view('notienespermisos');
+  }
+}
 
-            $otro = $this->m_usuarios->consulta_info_alumno($data['username']);
-            foreach ($otro as $value) {
-                $tmp = $value->correo;
-            }
-            $data['correo'] = $tmp;
+function actualizar_docente() {
+  $datos = array(
+    //'contrasena' => sha1($this->input->post('contrasena'))
+    'contrasena' => $this->input->post('contrasena')
+  );
+  if ($this->m_usuarios->actualizar_info_docente($this->session->userdata('rfc'), $datos)) {
+    redirect(base_url() . 'index.php/c_info_usuarios/docente');
+  } else {
+    $this->load->view('notienespermisos');
+  }
+}
 
-            $this->load->view('v_info_alumno', $data);
-        } else {
-            //If no session, redirect to login page
-            redirect('logeo', 'refresh');
-        }
+//ALUMNOS------------------------------------------------------------------------------
+public function alumno() {
+  if ($this->session->userdata('logged_in')) {
+    $session_data = $this->session->userdata('logged_in');
+    $data['username'] = $session_data['username'];
+    $data['nombre'] = $session_data['nombre'];
+    $data['id_carrera'] = $session_data['id_carrera'];
+    $data['id_semestre'] = $session_data['id_semestre'];
+    $data['plan_estudios'] = $session_data['plan_estudios'];
+    $data['sexo'] = $session_data['sexo'];
+    $data['telefono'] = $session_data['telefono'];
+    $data['domicilio'] = $session_data['domicilio'];
+    $data['semestre_cursado'] = $session_data['semestre_cursado'];
+    $data['creditos'] = $session_data['creditos'];
+    $data['porcentaje_avance'] = $session_data['porcentaje_avance'];
+
+    $otro = $this->m_usuarios->consulta_info_alumno($data['username']);
+    foreach ($otro as $value) {
+      $tmp = $value->correo;
     }
+    $data['correo'] = $tmp;
 
-    public function actualizar_alumno() {
-        if ($this->session->userdata('logged_in')) {
-            $session_data = $this->session->userdata('logged_in');
-            $data['username'] = $session_data['username'];
-            $data['nombre'] = $session_data['nombre'];
-            $data['id_carrera'] = $session_data['id_carrera'];
-            $data['id_semestre'] = $session_data['id_semestre'];
-            $data['plan_estudios'] = $session_data['plan_estudios'];
-            $data['sexo'] = $session_data['sexo'];
-            $data['telefono'] = $session_data['telefono'];
-            $data['domicilio'] = $session_data['domicilio'];
-            $data['semestre_cursado'] = $session_data['semestre_cursado'];
-            $data['creditos'] = $session_data['creditos'];
-            $data['porcentaje_avance'] = $session_data['porcentaje_avance'];
-        }
+    $this->load->view('v_info_alumno', $data);
+  } else {
+    //If no session, redirect to login page
+    redirect('logeo', 'refresh');
+  }
+}
 
-        $datos = array(
-            'correo' => $this->input->post('correo')
-        );
-        $this->m_usuarios->actualizar_alumno($data['username'], $datos);
+public function actualizar_alumno() {
+  if ($this->session->userdata('logged_in')) {
+    $session_data = $this->session->userdata('logged_in');
+    $data['username'] = $session_data['username'];
+    $data['nombre'] = $session_data['nombre'];
+    $data['id_carrera'] = $session_data['id_carrera'];
+    $data['id_semestre'] = $session_data['id_semestre'];
+    $data['plan_estudios'] = $session_data['plan_estudios'];
+    $data['sexo'] = $session_data['sexo'];
+    $data['telefono'] = $session_data['telefono'];
+    $data['domicilio'] = $session_data['domicilio'];
+    $data['semestre_cursado'] = $session_data['semestre_cursado'];
+    $data['creditos'] = $session_data['creditos'];
+    $data['porcentaje_avance'] = $session_data['porcentaje_avance'];
+  }
 
-        $otro = $this->m_usuarios->consulta_info_alumno($data['username']);
-        foreach ($otro as $value) {
-            $tmp = $value->correo;
-        }
-        $data['correo'] = $tmp;
-//        if ($data['correo'] != null) {
-//            $this->load->view('Residencia/Alumno/v_banco', $data);
-//        } else {
-//            $this->load->view('Residencia/Alumno/v_banco', $data);
-//        }
-        redirect('Residencia/Alumno/C_banco');
-    }
+  $datos = array(
+    'correo' => $this->input->post('correo')
+  );
+  $this->m_usuarios->actualizar_alumno($data['username'], $datos);
 
+  $otro = $this->m_usuarios->consulta_info_alumno($data['username']);
+  foreach ($otro as $value) {
+    $tmp = $value->correo;
+  }
+  $data['correo'] = $tmp;
+  //        if ($data['correo'] != null) {
+  //            $this->load->view('Residencia/Alumno/v_banco', $data);
+  //        } else {
+  //            $this->load->view('Residencia/Alumno/v_banco', $data);
+  //        }
+  redirect('Residencia/Alumno/C_banco');
+}
 }
--- application/views/v_info_administrativo.php
+++ application/views/v_info_administrativo.php
@@ -1,108 +1,145 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-        <title>SIV :: PANEL MI USUARIO-:</title>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>SIV :: PANEL MI USUARIO-:</title>
 
-        <!-- CSS  -->
+  <!-- CSS  -->
 
-        <link rel="shortcut icon" href="<?php echo base_url(); ?>images/favicon.png"/>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="shortcut icon" href="<?php echo base_url(); ?>images/favicon.png"/>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-        <!-- Compiled and minified CSS -->
-        <!--<link rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.min.css">-->
+  <!-- Compiled and minified CSS -->
+  <!--<link rel="stylesheet" href="<?php echo base_url(); ?>css/materialize.min.css">-->
 
-        <link rel="stylesheet" href="<?php echo base_url(); ?>js/DataTables/media/css/jquery.dataTables.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>js/DataTables/media/css/jquery.dataTables.css">
 
-        <link href="<?php echo base_url(); ?>css/materializesinselect.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-        <link href="<?php echo base_url(); ?>css/styles.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?php echo base_url(); ?>css/materializesinselect.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?php echo base_url(); ?>css/styles.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 
-    </head>
-    <body>
-        <nav>
-            <div class="nav-wrapper grey lighten-5 left-align">
-                <a href="#!" class="brand-logo center"><img src="<?php echo base_url(); ?>images/logochico.png" alt="Logo" /></a>
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper grey lighten-5 left-align">
+      <a href="#!" class="brand-logo center"><img src="<?php echo base_url(); ?>images/logochico.png" alt="Logo" /></a>
 
-                <div class="right-align hide-on-med-and-down">
-                    <a href="#"><div class=""></div><span class="grey-text text-darken-2 right-align hide-on-med-and-down"><?php echo $info; ?>
-                            <?= anchor(base_url() . 'index.php/Logeo/logout_ci', '<span class=" amber-text  right-align hide-on-med-and-down">(Cerrar sesión)  </span>') ?></span></a>
-                </div>
+      <div class="right-align hide-on-med-and-down">
+        <a href="#"><div class=""></div><span class="grey-text text-darken-2 right-align hide-on-med-and-down"><?php echo $info; ?>
+          <?= anchor(base_url() . 'index.php/Logeo/logout_ci', '<span class=" amber-text  right-align hide-on-med-and-down">(Cerrar sesión)  </span>') ?></span></a>
+        </div>
+      </div>
+
+    </nav>
+    <!-- Navbar goes here -->
+    <div class="section no-pad-bot" id="index-banner">
+      <div class="row center">
+        <h5 class="condensed light header center amber-text darken-1-text">
+          MI USUARIO</h5>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="section">
+          <a class = "tooltipped" data-position="top" data-delay="50" data-tooltip="Regresar" href="<?php echo base_url(); ?>index.php">
+            <img src="<?php echo base_url(); ?>images/keyboard_return_tiny.png"></a>
+            <a class="waves-effect waves-light btn modal-trigger orange " style="margin-left:5%; margin-bottom:3%;" href="#modal1"><i class="material-icons">edit</i>Cambiar Contraseña</a>
+            <?php if (isset($messageerror)) { ?>
+              <CENTER><p class="col s4 center-align card-panel red white-text"><?php echo "$messageerror"; ?></p></CENTER><br>
+            <?php } ?>
+            <div class="center-align col m6 card-panel grey lighten-5">
+              <table class="bordered highlight responsive-table">
+                <tr>
+                  <th>USUARIO</th>
+                  <th>NOMBRE DE USUARIO</th>
+                  <th>CORREO</th>
+                  <th>MODIFICAR</th>
+
+                </tr>
+                <?php
+                $usuarionombre="";
+                if ($info_usuario) {
+
+                  foreach ($info_usuario as $value) {
+                      $usuarionombre=$value->nombre_usuariosistema;
+                    ?>
+                    <tr>
+                      <form method="post" action="<?php echo base_url() . 'index.php/c_info_usuarios/actualizar_administrativo/' ?>">
+                        <td><?php echo $info; ?></td>
+                        <td><input type="text" name="usuario" id="usuario" value="<?php echo $value->nombre_usuariosistema; ?>" required/></td>
+                        <td><input type="text" pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@ittepic.edu.mx" title="...@ittepic.edu.mx" name="correo" id="correo" value="<?php echo $value->correo; ?>" required/></td>
+                        <td><button type="submit" style=" border: 0; background: transparent"><img src="<?php echo base_url(); ?>images/button-bg.png" width="0" height="0" alt="submit" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="material-icons blue-text">autorenew</i>
+                        </button></td>
+                      </form>
+                    </tr>
+                    <?php
+                  }
+                }
+                ?>
+              </table>
+
             </div>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+          </div>
+          <!-- MODAL CAMBIAR CONTRASEÑA -->
+          <?php echo form_open('C_info_usuarios/validarEditarContrasena'); ?>
+          <div id="modal1" class="modal">
+            <div class="modal-content">
+              <p class="amber-text" style=" font-size: 120%;">
+                <i class="material-icons">enhanced_encryption</i> Cambiar contraseña para <?php echo $usuarionombre;?></p>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input placeholder="Ingresar Contraseña Actual" id="contraacutal" name="contraacutal" type="text" value="<?php echo set_value('contraacutal'); ?>" class="validate" required>
+                  <label for="contraacutal">Contraseña Actual</label>
+                  <div class="red-text left"><?php echo form_error('contraacutal'); ?></div>
+                </div>
+                <div class="input-field col s6">
+                  <input id="contranueva1"  name="contranueva1" placeholder="Ingresar nueva contraseña." type="text" value="<?php echo set_value('contranueva1'); ?>" class="validate" required>
+                  <label for="contranueva1">Nueva Contraseña</label>
+                    <div class="red-text left"><?php echo form_error('contranueva1'); ?></div>
+                </div>
+                <div class="input-field col s6">
+                  <input id="contranueva2" type="text"  name="contranueva2" placeholder="Ingresar nuevamente la contraseña nueva." value="<?php echo set_value('contranueva2'); ?>" class="validate" required>
+                  <label for="contranueva2">Repetir Nueva Contraseña</label>
+                    <div class="red-text left"><?php echo form_error('contranueva2'); ?></div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <a href="#!" class="modal-action modal-close waves-effect waves-green btn red "><i class="material-icons">cancel</i>Cancelar</a>
+              <button class="btn waves-effect waves-green orange darken-1 right-align z-depth-0 " style="margin-right:2%;" type="submit">
+                <div class="text-orange"><i class="material-icons right">description</i>Cambiar Contraseña</div>
+              </button>
+            </div>
+          </div>
+          <?= form_close() ?>
+          <!-- MODAL CAMBIAR CONTRASEÑA -->
 
-        </nav>
-        <!-- Navbar goes here -->
-
-        <div class="section no-pad-bot" id="index-banner">            
-            <div class="row center">
-                <h5 class="condensed light header center amber-text darken-1-text">      
-                    MI USUARIO</h5>
-            </div>                
 
         </div>
-
-        <div class="container">
-            <div class="section">             
-<a class = "tooltipped" data-position="top" data-delay="50" data-tooltip="Regresar" href="<?php echo base_url(); ?>index.php">
-                <img src="<?php echo base_url(); ?>images/keyboard_return_tiny.png"></a>
-                <div class="center-align col m6 card-panel grey lighten-5">
-                    <table class="bordered highlight responsive-table">                        
-                        <tr>
-                            <th>USUARIO</th>
-                            <th>NOMBRE DE USUARIO</th>
-                            <th>CORREO</th>
-                            <th>MODIFICAR</th>
-
-                        </tr>
-                        <?php
-                        if ($info_usuario) {
-                            foreach ($info_usuario as $value) {
-                                ?>
-                                <tr>
-                                <form method="post" action="<?php echo base_url() . 'index.php/c_info_usuarios/actualizar_administrativo/' ?>">
-                                    <td><?php echo $info; ?></td>
-                                    <td><input type="text" name="usuario" id="usuario" value="<?php echo $value->nombre_usuariosistema; ?>" required/></td>
-                                    <td><input type="text" pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@ittepic.edu.mx" title="...@ittepic.edu.mx" name="correo" id="correo" value="<?php echo $value->correo; ?>" required/></td>
-                                    <td><button type="submit" style=" border: 0; background: transparent"><img src="<?php echo base_url(); ?>images/button-bg.png" width="0" height="0" alt="submit" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="material-icons blue-text">autorenew</i>
-                                        </button></td>
-                                </form>
-                                </tr>
-                                <?php
-                            }
-                        }
-                        ?>
-                    </table>
-
-                </div>                                
-
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-            </div>
-
-        </div>                
-
         <br><br>
-
         <footer class="page-footer black">
-            <div class="container">
+          <div class="container">
 
+          </div>
+          <div class="footer-copyright">
+            <div>
+              <div align="center ">Copyright 2017 - <a class=" amber-text text-lighten-3" href="http://www.ittepic.edu.mx"><span class="amber-text">
+                ITTepic
+              </span></a></div>
             </div>
-            <div class="footer-copyright">
-                <div>
-                    <div align="center ">Copyright 2016 - <a class=" amber-text text-lighten-3" href="http://www.ittepic.edu.mx"><span class="amber-text">
-                                ITTepic
-                            </span></a></div>
-                </div>
-            </div>
+          </div>
         </footer>
-
-        <script src="<?php echo base_url(); ?>js/jquery-3.0.0.min.js"></script>                       
-        <script src="<?php echo base_url(); ?>js/materialize.js"></script>                                     
+        <script src="<?php echo base_url(); ?>js/jquery-3.0.0.min.js"></script>
+        <script src="<?php echo base_url(); ?>js/materialize.js"></script>
+        <script src="<?php echo base_url(); ?>js/init.js"></script>
+        <script src="<?php echo base_url(); ?>js/modals.js"></script>
         <script type="text/javascript" charset="utf8" src="<?php echo base_url(); ?>/js/funciones.js"></script>
 
 
-    </body>
-</html>
+      </body>
+      </html>
