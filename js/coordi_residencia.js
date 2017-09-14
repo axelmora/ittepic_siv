dfd59a887f682c32f766842e58d51452a714fb83
fernando <feranimaciones@gmail.com>
2017-09-14 13:27:28 -0600

    css

--- js/coordi_residencia.js
+++ js/coordi_residencia.js
@@ -1,59 +1,105 @@
 $(document).ready(function(){
-   $('#btn_sel_docente').click(function(){      
+  $('#btn_sel_docente').click(function(){
 
-        var options = {
-            beforeSend: function ()
-            {
-                //beforeSend : this function called before form submission  
-            },
-            uploadProgress: function (event, position, total, percentComplete)
-            {
-                //uploadProgress : this function called when the upload is in progress        
-            },
-            success: function ()
-            {
-                //success : this function is called when the form upload is successful.
-            },
-            complete: function (response)
-            {
-                //complete: this function is called when the form upload is completed.  
-                //alert(response.responseText);
-                var a = jQuery.parseJSON(response.responseText);
-                if (a.parti_asesor==false && a.parti_revisor1==false && a.parti_revisor2==false) {
-                    alert('El docente no tiene participacion en proyectos.');
-                }
-                else{
-                    $('#participaciones').attr('hidden', false);
-                
-                var trHTML = '';
-                $.each(a.parti_asesor, function (i, item) {                    
-                        trHTML += '<tr><td>Asesor'+
-                            '</td><td>' + a.parti_asesor[i].nombre_proyecto+
-                            '</td></tr>';                                        
-                });
-                $.each(a.parti_revisor1, function (i, item) {                    
-                        trHTML += '<tr><td>Revisor1' +
-                            '</td><td>' + a.parti_revisor1[i].nombre_proyecto+
-                            '</td></tr>';                                        
-                });
-                $.each(a.parti_revisor2, function (i, item) {                    
-                        trHTML += '<tr><td>Revisor2' +
-                            '</td><td>' + a.parti_revisor2[i].nombre_proyecto+
-                            '</td></tr>';                                        
-                });
+    var options = {
+      beforeSend: function ()
+      {
+        //beforeSend : this function called before form submission
+      },
+      uploadProgress: function (event, position, total, percentComplete)
+      {
+        //uploadProgress : this function called when the upload is in progress
+      },
+      success: function ()
+      {
+        //success : this function is called when the form upload is successful.
+      },
+      complete: function (response)
+      {
+        //complete: this function is called when the form upload is completed.
+        //alert(response.responseText);
+        var a = jQuery.parseJSON(response.responseText);
+        if (a.parti_asesor==false && a.parti_revisor1==false && a.parti_revisor2==false) {
+          alert('El docente no tiene participacion en proyectos.');
+          $('#participaciones').attr('hidden', true);
+        }
+        else{
+          $('#participaciones').attr('hidden', false);
 
-                $('#tabla_participantes_proy tbody').html(trHTML);
-                }
-                
-                                                
-            },
-            error: function ()
-            {
-                alert('Error1');
-            }
-        };
+          var trHTML = '';
+          $.each(a.parti_asesor, function (i, item) {
+            trHTML += '<tr><td>Asesor'+
+            '</td><td>' + a.parti_asesor[i].nombre_proyecto+
+            '</td></tr>';
+          });
+          $.each(a.parti_revisor1, function (i, item) {
+            trHTML += '<tr><td>Revisor1' +
+            '</td><td>' + a.parti_revisor1[i].nombre_proyecto+
+            '</td></tr>';
+          });
+          $.each(a.parti_revisor2, function (i, item) {
+            trHTML += '<tr><td>Revisor2' +
+            '</td><td>' + a.parti_revisor2[i].nombre_proyecto+
+            '</td></tr>';
+          });
 
-        $("#frm_sel_docente").ajaxForm(options);     
-             
-   }); 
+          $('#tabla_participantes_proy tbody').html(trHTML);
+        }
+      },
+      error: function ()
+      {
+        alert('Error1');
+      }
+    };
+    $("#frm_sel_docente").ajaxForm(options);
+  });
+  /*DOCENTE COMPARTIDO*/
+  $('#btn_sel_docente2').click(function(){
+
+    var options = {
+      beforeSend: function ()
+      {
+      },
+      uploadProgress: function (event, position, total, percentComplete)
+      {
+      },
+      success: function ()
+      {
+      },
+      complete: function (response)
+      {
+        var a = jQuery.parseJSON(response.responseText);
+        if (a.parti_asesor==false && a.parti_revisor1==false && a.parti_revisor2==false) {
+          alert('El docente compartido  no tiene participacion en proyectos.');
+            $('#participaciones').attr('hidden', true);
+        }
+        else{
+          $('#participaciones').attr('hidden', false);
+          var trHTML = '';
+          $.each(a.parti_asesor, function (i, item) {
+            trHTML += '<tr><td>Asesor'+
+            '</td><td>' + a.parti_asesor[i].nombre_proyecto+
+            '</td></tr>';
+          });
+          $.each(a.parti_revisor1, function (i, item) {
+            trHTML += '<tr><td>Revisor1' +
+            '</td><td>' + a.parti_revisor1[i].nombre_proyecto+
+            '</td></tr>';
+          });
+          $.each(a.parti_revisor2, function (i, item) {
+            trHTML += '<tr><td>Revisor2' +
+            '</td><td>' + a.parti_revisor2[i].nombre_proyecto+
+            '</td></tr>';
+          });
+          $('#tabla_participantes_proy tbody').html(trHTML);
+        }
+      },
+      error: function ()
+      {
+        alert('Error1');
+      }
+    };
+    $("#frm_sel_docentecompartido").ajaxForm(options);
+  });
+  /*DOCENTE COMPARTIDO*/
 });
