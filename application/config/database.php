831d8b676101f82e4096a9164913acbb54d7ba5f
fernando <feranimaciones@gmail.com>
2017-09-29 12:12:47 -0600

    ACT final

--- application/config/database.php
+++ application/config/database.php
@@ -62,6 +62,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'local';
 $query_builder = TRUE;
 
+$db['default']['hostname'] = "localhost";
+$db['default']['username'] = "postgres";
+$db['default']['password'] = "1234";
+$db['default']['database'] = "bd_siv";
+$db['default']['dbdriver'] = "postgre";
+$db['default']['port']   = '5432';
+$db['default']['dbprefix'] = "";
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = FALSE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = "";
+$db['default']['char_set'] = "utf8";
+$db['default']['dbcollat'] = "utf8_general_ci";
+$db['default']['swap_pre'] = "";
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
+
+$db['local']['hostname'] = "localhost";
+$db['local']['username'] = "postgres";
+$db['local']['password'] = "1234";
+$db['local']['database'] = "bd_siv";
+$db['local']['dbdriver'] = "postgre";
+$db['local']['port']   = '5432';
+$db['local']['dbprefix'] = "";
+$db['local']['pconnect'] = TRUE;
+$db['local']['db_debug'] = FALSE;
+$db['local']['cache_on'] = FALSE;
+$db['local']['cachedir'] = "";
+$db['local']['char_set'] = "utf8";
+$db['local']['dbcollat'] = "utf8_general_ci";
+$db['local']['swap_pre'] = "";
+$db['local']['autoinit'] = TRUE;
+$db['local']['stricton'] = FALSE;
+
+
 /*
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "postgres";
@@ -101,15 +136,15 @@ $db['local']['stricton'] = FALSE;
 /*
 $db['local']['hostname'] = "172.28.0.2";
 */
-
+/*
 $db['local']['hostname'] = "localhost";
 $db['local']['username'] = "siv";
 $db['local']['password'] = "di51vdi51v";
 $db['local']['database'] = "bd_siv";
 $db['local']['dbdriver'] = "postgre";
-$db['local']['port']   = '5432';
+$db['local']['port']   = '5432';*/
 /*$db['local']['port']   = '5433';*/
-
+/*
 $db['local']['dbprefix'] = "";
 $db['local']['pconnect'] = TRUE;
 $db['local']['db_debug'] = FALSE;
@@ -137,7 +172,7 @@ $db['default']['dbcollat'] = "utf8_general_ci";
 $db['default']['swap_pre'] = "";
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-
+*/
 
 
 /*-----------------------------------------------------
