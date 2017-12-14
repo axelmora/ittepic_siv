0b782b9d8444aeba9933828e312244db59cd50c3
fernando <feranimaciones@gmail.com>
2017-12-13 19:16:12 -0700

    ACT

--- application/controllers/Residencia/Alumno/C_proponer.php
+++ application/controllers/Residencia/Alumno/C_proponer.php
@@ -20,8 +20,8 @@ class C_proponer extends CI_Controller {
     if ($this->session->userdata('logged_in')) {
   /*    echo ini_get('upload_max_filesize').'<br/>';
       echo ini_get("post_max_size");
-      phpinfo();*/
-      echo CI_VERSION;
+      phpinfo();
+      echo CI_VERSION;*/
       $session_data = $this->session->userdata('logged_in');
       $data['username'] = $session_data['username'];
       $data['nombre'] = $session_data['nombre'];
